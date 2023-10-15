d17e8d806a11f94dd5f2bcba6ccd35f7e6f7afaa
2022526a <2022526@student.cct.ie>
2023-10-15 20:08:21 +0100

    Airplane  Class Extending Vehicle
    Airplane class extending vehicle implementing flyable. Inheriting properties . Calling  constructors .Adding method to change the altitude. Adding method to get current   altitude.

--- src/oocminihw2/Airplane.java
+++ src/oocminihw2/Airplane.java
@@ -8,9 +8,25 @@ package oocminihw2;
  *
  * @author 35389
  */
-public class Airplane extends Vehicle implements Flyable {
+public abstract  class Airplane extends Vehicle implements Flyable {
     public Airplane(float speed, String make, String type, int numpassengers){
         super(speed, make,type, numpassengers);
         numWings = 2;
     }
+    @Override
+    public void changeAltitude(float change){
+        String altitude = null;
+    altitude +=change;
+        System.out.println("Airplane altitude changed to" + altitude + "feet.");
+    
+    }
+    @Override
+    public float getAltitude(){
+        float altitude = 0;
+        return altitude;
+    }
 }
+    
+    
+    
+
