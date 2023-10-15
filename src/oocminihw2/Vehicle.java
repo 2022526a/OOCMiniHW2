151493dee15fe0f6bcba6c5424072d89a9388f44
2022526a <2022526@student.cct.ie>
2023-10-15 17:13:35 +0100

    Update Vehicle.java
    Adding Getters And Setters. Implementing Methods.

--- src/oocminihw2/Vehicle.java
+++ src/oocminihw2/Vehicle.java
@@ -17,4 +17,75 @@ public abstract class Vehicle {
     protected int numWings = 0;
     protected int numSails = 0;
     private int numPassengers;
+    public Vehicle (float speed, String make, String type, int numpassengers){
+        this.speed= speed;
+        this.make= make;
+        this.type= type;
+        this.numPassengers=numpassengers;
+}
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getDirection() {
+        return direction;
+    }
+
+    public void setDirection(float direction) {
+        this.direction = direction;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumWheels() {
+        return numWheels;
+    }
+
+    public void setNumWheels(int numWheels) {
+        this.numWheels = numWheels;
+    }
+
+    public int getNumWings() {
+        return numWings;
+    }
+
+    public void setNumWings(int numWings) {
+        this.numWings = numWings;
+    }
+
+    public int getNumSails() {
+        return numSails;
+    }
+
+    public void setNumSails(int numSails) {
+        this.numSails = numSails;
+    }
+
+    public int getNumPassengers() {
+        return numPassengers;
+    }
+
+    public void setNumPassengers(int numPassengers) {
+        this.numPassengers = numPassengers;
+    }
+    
 }
