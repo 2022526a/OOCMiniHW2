5f4d1d4f6439429e56ef8af01d7701eeea1b04b7
2022526a <2022526@student.cct.ie>
2023-10-15 17:26:50 +0100

    sailboat extend vehicle implements sailable
    extending and implementing

--- src/oocminihw2/Sailboat.java
+++ src/oocminihw2/Sailboat.java
@@ -8,8 +8,30 @@ package oocminihw2;
  *
  * @author 35389
  */
-public class Sailboat extends Vehicle  {
-    public Sailboat(String make, String type, float speed,int numpassengers){
-    
-}
-}
+public class Sailboat extends Vehicle implements Sailable  {
+    private boolean sailhosted;
+    private boolean sailHoisted;
+  public Sailboat(float speed,String make, String type,int numpassengers) {
+      super(speed,make,type,numpassengers);
+      numSails = 1;
+    }
+  @Override
+  public void hoistSail(){
+      
+      System.out.println("Sail has been hoisted");
+  }
+  @Override
+  public void lowerSail(){
+      System.out.println("Sail has been lowered");
+  }
+  @Override
+  public boolean isSailHoisted(){
+      return sailHoisted; 
+  }
+  @Override
+  public void landHo(){
+      System.out.println("Sailboat is landing.");
+  }
+      
+  }
+  
